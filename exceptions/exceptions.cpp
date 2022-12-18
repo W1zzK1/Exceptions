df30beee20659fc5be1875e450c3f56322ab20b3
W1zzK1 <vladgorr1262@gmail.com>
2022-12-19 00:28:46 +0300

    main

---
+++ exceptions/exceptions.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "date.h" 
+using namespace std;
+
+int main()
+{
+    using namespace date;
+    int a;
+    cin >> a;
+    int b;
+    cin >> b;
+    try
+    {
+        sys_days birthday;
+        sys_days today = floor<days>(std::chrono::system_clock::now());
+        cout << (today - birthday).count() << endl;
+    }
+    catch (...) {
+        cout << "Данные введены не коректно";
+    }
+}
